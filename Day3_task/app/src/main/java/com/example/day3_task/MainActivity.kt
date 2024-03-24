fa1bd28fdf7b9c7ec792a0d892ae85968581945d
InternetGirl <trangtt181202@gmail.com>
2024-03-24 20:18:06 +0700

    calculator_operator

--- Day3_task/app/src/main/java/com/example/day3_task/MainActivity.kt
+++ Day3_task/app/src/main/java/com/example/day3_task/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.day3_task
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,18 +9,16 @@ import androidx.appcompat.widget.AppCompatImageButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultTextView: TextView
-    private var currentNumber = ""
-    private var firstOperand = 0.0
-    private var operator = ""
+    private var number: String = ""
+    private lateinit var tvResult: TextView
+    private var firstNumber: Int = 0
+    private var operator: Char = ' '
 
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        resultTextView = findViewById(R.id.resultTextView)
 
         val btnNumberZero: AppCompatImageButton = findViewById(R.id.number_zero)
         val btnNumberOne: AppCompatImageButton = findViewById(R.id.number_one)
@@ -38,96 +35,105 @@ class MainActivity : AppCompatActivity() {
         val btnMinus: AppCompatImageButton = findViewById(R.id.btn_minus)
         val btnMulti: AppCompatImageButton = findViewById(R.id.btn_multi)
         val btnDiv: AppCompatImageButton = findViewById(R.id.btn_div)
+
         val btnEqual: AppCompatImageButton = findViewById(R.id.btn_equal)
+        val btnCleaner: AppCompatImageButton = findViewById(R.id.del_btn)
+
+        tvResult  = findViewById(R.id.textViewResult)
 
         btnNumberZero.setOnClickListener {
-            appendNumber("0")
+            numberClick("0")
         }
         btnNumberOne.setOnClickListener {
-            appendNumber("1")
+            numberClick("1")
         }
         btnNumberTwo.setOnClickListener {
-            appendNumber("2")
+            numberClick("2")
         }
         btnNumberThree.setOnClickListener {
-            appendNumber("3")
+            numberClick("3")
         }
         btnNumberFour.setOnClickListener {
-            appendNumber("4")
+            numberClick("4")
         }
         btnNumberFive.setOnClickListener {
-            appendNumber("5")
+            numberClick("5")
         }
         btnNumberSix.setOnClickListener {
-            appendNumber("6")
+            numberClick("6")
         }
         btnNumberSeven.setOnClickListener {
-            appendNumber("7")
+            numberClick("7")
         }
         btnNumberEight.setOnClickListener {
-            appendNumber("8")
+            numberClick("8")
         }
         btnNumberNine.setOnClickListener {
-            appendNumber("9")
+            numberClick("9")
+        }
+
+        btnAdd.setOnClickListener() {
+            operatorClick('+')
+        }
+        btnMinus.setOnClickListener() {
+            operatorClick('-')
+        }
+        btnMulti.setOnClickListener() {
+            operatorClick('*')
+        }
+        btnDiv.setOnClickListener() {
+            operatorClick('/')
+        }
+
+        btnEqual.setOnClickListener() {
+            equalClick()
+        }
+        btnCleaner.setOnClickListener(){
+            clearCalculator()
+        }
+    }
+    //click button number
+    private fun numberClick(n: String) {
+        number += n
+        tvResult.text = number
+    }
+
+    //click button operator
+    private fun operatorClick(op: Char) {
+        if (number.isNotEmpty()) {
+            firstNumber = number.toInt()
+            number = ""
+            operator = op
+            tvResult.text = firstNumber.toString()
         }
     }
 
-    // nhập số đầu tiên
-    private fun appendNumber(number: String) {
-        currentNumber += number
-        resultTextView.text = currentNumber
+    //Display result
+    private fun equalClick() {
+        if (number.isNotEmpty() && operator != ' ') {
+            val secondNumber = number.toInt()
+            var result = 0
+            when (operator) {
+                '+' -> result = firstNumber + secondNumber
+                '-' -> result = firstNumber - secondNumber
+                '*' -> result = firstNumber * secondNumber
+                '/' -> {
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber
+                    }
+                }
+            }
+            tvResult.text = result.toString()
+            number = ""
+            operator = ' '
+        }
     }
 
-
-//    private fun performOperation(op: String) {
-//        if (currentNumber.isNotEmpty()) {
-//            firstOperand = currentNumber.toDouble()
-//            operator = op
-//            currentNumber = ""
-//        }
-//    }
+    private fun clearCalculator() {
+        number = ""
+        firstNumber = 0
+        operator = ' '
+        tvResult.text = ""
+        tvResult.text = ""
+    }
 }
-
-//        // Tương tự cho các nút số từ 1 đến 9 và nút chấm (buttonDot)
-//
-//        btnAdd.setOnClickListener {
-//            performOperation("+")
-//        }
-//
-//        // Tương tự cho các nút phép tính trừ, nhân, chia
-//
-//        btnEqual.setOnClickListener {
-//            calculateResult()
-//        }
-//
-////        buttonClear.setOnClickListener {
-////            clearCalculator()
-////        }
-
-
-
-//
-//    private fun calculateResult() {
-//        if (currentNumber.isNotEmpty()) {
-//            val secondOperand = currentNumber.toDouble()
-//            var result = 0.0
-//
-//            when (operator) {
-//                "+" -> result = firstOperand + secondOperand
-//                "-" -> result = firstOperand - secondOperand
-//                "*" -> result = firstOperand * secondOperand
-//                "/" -> result = firstOperand / secondOperand
-//            }
-//
-//            currentNumber = result.toString()
-//            resultTextView.text = currentNumber
-//        }
-//    }
-//
-//    private fun clearCalculator() {
-//        currentNumber = ""
-//        firstOperand = 0.0
-//        operator = ""
-//        resultTextView.text = "0"
-//    }
-
